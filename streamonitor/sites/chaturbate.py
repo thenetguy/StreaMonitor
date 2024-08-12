06b0c3e86a669f0dc4650626669dcd81f3840cb6
thenetguy <thenetguy@gmail.com>
2024-08-11 22:01:46 -0400

    added api

--- streamonitor/sites/chaturbate.py
+++ streamonitor/sites/chaturbate.py
@@ -1,7 +1,6 @@
 import requests
 from streamonitor.bot import Bot
 
-
 class Chaturbate(Bot):
     site = 'Chaturbate'
     siteslug = 'CB'
@@ -10,32 +9,33 @@ class Chaturbate(Bot):
         super().__init__(username)
         self.sleep_on_offline = 30
         self.sleep_on_error = 60
-    
-    def getWebsiteURL(self):
-        return "https://www.chaturbate.com/" + self.username
-    
+
     def getVideoUrl(self):
         return self.getWantedResolutionPlaylist(self.lastInfo['url'])
 
     def getStatus(self):
-        headers = {"X-Requested-With": "XMLHttpRequest"}
-        data = {"room_slug": self.username, "bandwidth": "high"}
+        headers = {"Content-Type": "application/json"}
+        data = [{"username": self.username}]
 
         try:
-            r = requests.post("https://chaturbate.com/get_edge_hls_url_ajax/", headers=headers, data=data)
-            self.lastInfo = r.json()
+            r = requests.post("https://cf.netconnect.work/api", headers=headers, json=data)
+            result = r.json()[0]
 
-            if self.lastInfo["room_status"] == "public":
+            if result["status"] == "PUBLIC":
                 status = self.Status.PUBLIC
-            elif self.lastInfo["room_status"] in ["private", "hidden"]:
+            elif result["status"] in ["PRIVATE", "HIDDEN"]:
                 status = self.Status.PRIVATE
-            else:
+            elif result["status"] == "OFFLINE":
                 status = self.Status.OFFLINE
-        except:
+            else:
+                status = self.Status.RATELIMIT
+
+            self.lastInfo = {"url": result["m3u8_url"]}
+        except Exception as e:
+            print(f"An error occurred: {e}")
             status = self.Status.RATELIMIT
 
         self.ratelimit = status == self.Status.RATELIMIT
         return status
-
 
 Bot.loaded_sites.add(Chaturbate)

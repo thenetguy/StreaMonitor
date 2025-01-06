ea9a140c760d6d0022e35474704a0d9f2702d409
thenetguy <thenetguy@gmail.com>
2025-01-05 19:34:31 -0500

    Update ffmpeg.py

--- streamonitor/downloaders/ffmpeg.py
+++ streamonitor/downloaders/ffmpeg.py
@@ -4,98 +4,118 @@ import sys
 import requests.cookies
 from threading import Thread
 from parameters import DEBUG, SEGMENT_TIME, CONTAINER
+from datetime import datetime
+import os
 
 def getVideoFfmpeg(self, url, filename):
-   cmd = [
-       'ffmpeg',
-       '-user_agent', self.headers['User-Agent']
-   ]
-   
-   if type(self.cookies) is requests.cookies.RequestsCookieJar:
-       cookies_text = ''
-       for cookie in self.cookies:
-           cookies_text += cookie.name + "=" + cookie.value + "; path=" + cookie.path + '; domain=' + cookie.domain + '\n'
-       if len(cookies_text) > 10:
-           cookies_text = cookies_text[:-1]
-       cmd.extend([
-           '-cookies', cookies_text
-       ])
-   
-   cmd.extend([
-       '-i', url,
-       '-fflags', '+genpts',
-       '-copytb', '1',  # Use demuxer timebase
-       '-c:a', 'copy',
-       '-c:v', 'copy',
-   ])
+    cmd = [
+        'ffmpeg',
+        '-user_agent', self.headers['User-Agent']
+    ]
+    
+    if type(self.cookies) is requests.cookies.RequestsCookieJar:
+        cookies_text = ''
+        for cookie in self.cookies:
+            cookies_text += cookie.name + "=" + cookie.value + "; path=" + cookie.path + '; domain=' + cookie.domain + '\n'
+        if len(cookies_text) > 10:
+            cookies_text = cookies_text[:-1]
+        cmd.extend([
+            '-cookies', cookies_text
+        ])
+    
+    cmd.extend([
+        '-i', url,
+        '-c:a', 'copy',
+        '-c:v', 'copy',
+    ])
 
-   if SEGMENT_TIME is not None:
-       username = filename.rsplit('-', maxsplit=2)[0]
-       cmd.extend([
-           '-f', 'segment',
-           '-start_at_zero',
-           '-reset_timestamps', '1',
-           '-segment_time', str(SEGMENT_TIME),
-           '-strftime', '1',
-           f'{username}-%Y%m%d-%H%M%S.{CONTAINER}'
-       ])
-   else:
-       cmd.extend([
-           filename
-       ])
+    if SEGMENT_TIME is not None:
+        username = filename.rsplit('-', maxsplit=2)[0]
+        temp_name = f'{username}_%d.{CONTAINER}'  # Use numbered segments temporarily
+        cmd.extend([
+            '-f', 'segment',
+            '-segment_time', str(SEGMENT_TIME),
+            temp_name
+        ])
+    else:
+        cmd.extend([
+            filename
+        ])
 
-   class _Stopper:
-       def __init__(self):
-           self.stop = False
-       def pls_stop(self):
-           self.stop = True
+    class _Stopper:
+        def __init__(self):
+            self.stop = False
+        def pls_stop(self):
+            self.stop = True
 
-   stopping = _Stopper()
-   error = False
+    stopping = _Stopper()
+    error = False
 
-   def execute():
-       nonlocal error
-       try:
-           stdout = open(filename + '.stdout.log', 'w+') if DEBUG else subprocess.DEVNULL
-           stderr = open(filename + '.stderr.log', 'w+') if DEBUG else subprocess.DEVNULL
-           startupinfo = None
-           if sys.platform == "win32":
-               startupinfo = subprocess.STARTUPINFO()
-               startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-           process = subprocess.Popen(
-               args=cmd, 
-               stdin=subprocess.PIPE, 
-               stderr=stderr, 
-               stdout=stdout, 
-               startupinfo=startupinfo
-           )
-       except OSError as e:
-           if e.errno == errno.ENOENT:
-               self.logger.error('FFMpeg executable not found!')
-               error = True
-               return
-           else:
-               self.logger.error("Got OSError, errno: " + str(e.errno))
-               error = True
-               return
+    def execute():
+        nonlocal error
+        try:
+            stdout = open(filename + '.stdout.log', 'w+') if DEBUG else subprocess.DEVNULL
+            stderr = open(filename + '.stderr.log', 'w+') if DEBUG else subprocess.DEVNULL
+            startupinfo = None
+            if sys.platform == "win32":
+                startupinfo = subprocess.STARTUPINFO()
+                startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+            process = subprocess.Popen(
+                args=cmd, 
+                stdin=subprocess.PIPE, 
+                stderr=stderr, 
+                stdout=stdout, 
+                startupinfo=startupinfo
+            )
+            
+            # After each segment is created, rename it with current timestamp
+            if SEGMENT_TIME is not None:
+                while process.poll() is None:
+                    if stopping.stop:
+                        process.communicate(b'q')
+                        break
+                    try:
+                        process.wait(1)
+                        # Check for new segment files and rename them
+                        for f in os.listdir('.'):
+                            if f.startswith(username) and f.endswith(f'.{CONTAINER}'):
+                                if '_' in f:  # Only rename files with numbered format
+                                    now = datetime.now()
+                                    new_name = f'{username}-{now.strftime("%Y%m%d-%H%M%S")}.{CONTAINER}'
+                                    try:
+                                        os.rename(f, new_name)
+                                    except:
+                                        pass
+                    except subprocess.TimeoutExpired:
+                        pass
+            else:
+                while process.poll() is None:
+                    if stopping.stop:
+                        process.communicate(b'q')
+                        break
+                    try:
+                        process.wait(1)
+                    except subprocess.TimeoutExpired:
+                        pass
 
-       while process.poll() is None:
-           if stopping.stop:
-               process.communicate(b'q')
-               break
-           try:
-               process.wait(1)
-           except subprocess.TimeoutExpired:
-               pass
+        except OSError as e:
+            if e.errno == errno.ENOENT:
+                self.logger.error('FFMpeg executable not found!')
+                error = True
+                return
+            else:
+                self.logger.error("Got OSError, errno: " + str(e.errno))
+                error = True
+                return
 
-       if process.returncode and process.returncode != 0 and process.returncode != 255:
-           self.logger.error('The process exited with an error. Return code: ' + str(process.returncode))
-           error = True
-           return
+        if process.returncode and process.returncode != 0 and process.returncode != 255:
+            self.logger.error('The process exited with an error. Return code: ' + str(process.returncode))
+            error = True
+            return
 
-   thread = Thread(target=execute)
-   thread.start()
-   self.stopDownload = lambda: stopping.pls_stop()
-   thread.join()
-   self.stopDownload = None
-   return not error
+    thread = Thread(target=execute)
+    thread.start()
+    self.stopDownload = lambda: stopping.pls_stop()
+    thread.join()
+    self.stopDownload = None
+    return not error
